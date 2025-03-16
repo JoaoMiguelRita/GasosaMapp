36da21ce1718938cdd61034934642a42c07425fb
JoaoMiguel <joaomiguelrita01@gmail.com>
2025-03-16 19:28:36 -0300

    Migracao com banco Postgres

--- src/main/java/com/example/gasosamapp/GasosamappApplication.java
+++ src/main/java/com/example/gasosamapp/GasosamappApplication.java
@@ -2,9 +2,8 @@ package com.example.gasosamapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class GasosamappApplication {
 
 	public static void main(String[] args) {
